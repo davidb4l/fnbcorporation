58c3430ff4f46a59cb4648ad7642115b9e25c0cd
davidb4l <nightflyer-labs@proton.me>
2024-09-20 05:56:20 +0100

    second commit

--- FNBPA/scripts/connect.php
+++ FNBPA/scripts/connect.php
@@ -1,9 +1,10 @@
 <?php
-if (!isset($_ENV["PHP_ENV"])) {
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-}
+// !isset($_ENV["PHP_ENV"])
+// if () {
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// }
 $servername = "bqtog0adsgvasegsab1x-mysql.services.clever-cloud.com";
 $username = "uuzlj1u0e1neylgw";
 $dbname = "bqtog0adsgvasegsab1x";
